255c7072bd003554df130d05bb9728be05dc4712
Denis Kiselev <52455108+thexdk@users.noreply.github.com>
2023-04-29 01:34:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,9 +7,6 @@ from scipy.stats import erlang
 chat_id = 477096063 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
     n = x.size
     loc = x.mean()
